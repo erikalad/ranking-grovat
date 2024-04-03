6b0bd56fad6a9b622d01219854647194bfcfbd7c
Erika Ladner <erikaladner@MacBook-Air-de-Erika.local>
2024-04-03 18:58:32 -0300

    datos

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import Container from './Components/Container';
 import { ConfigProvider } from 'antd';
 import esES from 'antd/es/locale/es_ES';
-import { useDispatch } from 'react-redux';
-import { getRanking } from './redux/actions'; // Asegúrate de importar las acciones correctamente
+// import { useDispatch } from 'react-redux';
+// import { getRanking } from './redux/actions'; // Asegúrate de importar las acciones correctamente
 
 function App() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getRanking());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(getRanking());
+  // }, [dispatch]);
 
   return (
     <ConfigProvider locale={esES}>
